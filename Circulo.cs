10a85d33d4ea9d4ad68454551ec91bf4730bf5f8
Marcos Chaves <marcosdmchaves@gmail.com>
2026-03-06 09:50:34 -0300

    Adicionar arquivos de projeto.

---
+++ Circulo.cs
@@ -0,0 +1,24 @@
+﻿
+namespace FiguraGeometrica
+{
+    /// <summary>This class defines the area calculations for a circle of radius r</summary>
+    public class Circulo : FiguraGeometrica
+    {
+        private readonly float RaioDoCirculoEmUM;
+
+        public Circulo(string nomeDaFigura,
+                        float raio) : base(nomeDaFigura)
+        {
+            if(raio <= 0)
+            {
+                throw new ArgumentException($"Argumento '{nameof(raio)}'={raio} inválido!");
+            }
+            RaioDoCirculoEmUM = raio;
+        }
+
+        public float CalcularArea()
+        {
+            return (float)Math.PI * (RaioDoCirculoEmUM * RaioDoCirculoEmUM);
+        }
+    }
+}
---
+++ FiguraGeometrica.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FiguraGeometrica
+{
+    /// <summary>This class defines a base for Figuras Geométricas</summary>
+    public class FiguraGeometrica
+    {
+        private readonly string NomeDaFigura;
+
+        public FiguraGeometrica(string nomeDaFigura)
+        {
+            string sanitizedNomeDaFigura = SanitizeNomeDaFigura(nomeDaFigura: nomeDaFigura);
+
+            if (!IsNomeDaFiguraValid(nomeDaFigura: sanitizedNomeDaFigura))
+            {
+                throw new ArgumentException($"Argumento '{nameof(nomeDaFigura)}'='{nomeDaFigura}' inválido!");
+            }
+
+            NomeDaFigura = nomeDaFigura;
+        }
+
+        private bool IsNomeDaFiguraValid(string nomeDaFigura)
+        {
+            if (string.IsNullOrEmpty(nomeDaFigura) ||
+                string.IsNullOrWhiteSpace(nomeDaFigura))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private string SanitizeNomeDaFigura(string nomeDaFigura)
+        {
+            string pattern = @"[^\p{L}\s]";
+
+            // Use Regex.Replace to remove the characters
+            string sanitizedNomeDaFigura = Regex.Replace(nomeDaFigura, pattern, string.Empty);
+
+            return sanitizedNomeDaFigura;
+        }
+
+        public string GetNomeDaFigura()
+        {
+            return NomeDaFigura;
+        }
+
+    }
+}
